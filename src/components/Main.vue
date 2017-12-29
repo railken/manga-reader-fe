da2dd869c69c5c418f052906d24a05578c887927
Unknown <railken@protonmail.ch>
2017-12-29 15:38:16 +0100

    add logout

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,21 +1,37 @@
 <template>
   <div>
-  	<div v-if='user'>
-    	{{ user.username }}
+  	<div class='paper'>
+  		<div v-if='user'>
+    		{{ user.username }}
+    		<button class='btn btn-primary' v-on:click="logout()">Logout</button>
+		</div>
+		<div v-if="!user">
+
+    		<router-link class='btn btn-primary' :to="{ name: 'sign-in' }">Login</router-link>
+		</div>
 	</div>
   </div>
 </template>
 
 <script>
-export default {
-	name: 'Main',
-	props: ['user'],
-	data () {
-		return {
-		 	msg: ' ',
+
+	import { container } from "../services/container";
+
+	export default {
+		name: 'Main',
+		props: ['user'],
+		data () {
+			return {
+
+			}
+		},
+		methods: {
+			logout() {
+	        	container.get('services.oauth').logout();
+	        	window.location.href = "/";
+			}
 		}
 	}
-}
 </script>
 
 <style>
