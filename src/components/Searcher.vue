99029eb68f9f2d9067a01ad392ce0dbdcf139d95
Unknown <railken@protonmail.ch>
2018-02-22 17:15:59 +0100

    update searcher page

--- src/components/Searcher.vue
+++ src/components/Searcher.vue
@@ -3,35 +3,41 @@
 
     <div class='container'>
         <div class='paper page request'>
-            <form v-on:submit.prevent="search" class='fluid'>
-               
-                <input class='form-control' placeholder='Type your search here' v-model='query'>
-                
-                <button class='btn btn-primary' type='submit'>Search</button>
-                
+            <form v-on:submit.prevent="search">
+                <div  class='fluid'>
+                    <input class='form-control' placeholder='Type your search here' v-model='query'>
+                    
+                    <button class='btn btn-primary' type='submit'>Search</button>
+                </div>
+                <p>
 
+                </p>
+                <p>
+                    Tags
+                </p>
             </form>
         </div>
 
+        <div v-if="resources.length != 0">
+            <div  v-for="manga in resources" class="page paper paper-hover manga-container">
+                <div class='fluid'>
+                    <div class='cover-container'><router-link class='url' :to="{ name: 'manga', params: { slug: manga.slug }}"><img :src='manga.cover' class='img cover'></router-link></div>
+                    <div class='info'>
+                        <h3>
+                            <router-link class='url' :to="{ name: 'manga', params: { slug: manga.slug }}">{{ manga.title }}</router-link>
+                            <span v-if='manga.follow' class='fa fa-cloud-download manga-followed-icon' title='All new scans will be automatically downloaded'></span>
+                        </h3>
+                        
+                        <div class='info-basic' v-if='manga.genres'>
+                            {{ manga.genres.join(", ") }}
+                        </div>
 
-        <div v-if="result" v-for="manga in result.resources" class="page paper paper-hover manga-container">
-            <div class='fluid'>
-                <div class='cover-container'><router-link class='url' :to="{ name: 'manga', params: { slug: manga.slug }}"><img :src='manga.cover' class='img cover'></router-link></div>
-                <div class='info'>
-                    <h3>
-                        <router-link class='url' :to="{ name: 'manga', params: { slug: manga.slug }}">{{ manga.title }}</router-link>
-                        <span v-if='manga.follow' class='fa fa-cloud-download manga-followed-icon' title='All new scans will be automatically downloaded'></span>
-                    </h3>
-                    
-                    <div class='info-basic'>
-                        {{ manga.genres.join(", ") }}
+                        <div class='info-basic' v-if='manga.aliases'>
+                            <label>Aliases</label>
+                            {{ manga.aliases.join(", ") }}
+                        </div>
+                        <div>{{ manga.overview }}</div>
                     </div>
-
-                    <div class='info-basic'>
-                        <label>Aliases</label>
-                        {{ manga.aliases.join(", ") }}
-                    </div>
-                    <div>{{ manga.overview }}</div>
                 </div>
             </div>
         </div>
@@ -51,13 +57,33 @@ export default {
             manga: null,
             slug: null,
             query: '',
-            show: 5,
-            page: 1,
+            show: 15,
+            page: 0,
             result: {},
+            loading: false,
+            resources: [],
         };
     },
     props: ['user'],
     methods: {
+
+        handleScroll () {
+            var scrolled = window.scrollY > 0;
+
+            var totalHeight = document.documentElement.scrollHeight;
+            var clientHeight = document.documentElement.clientHeight;
+            var scrollTop = (document.body && document.body.scrollTop)
+            ? document.body.scrollTop : document.documentElement.scrollTop;
+
+            if (totalHeight - 200 <= scrollTop + clientHeight) {
+
+                if (!this.loading && this.result && this.result.pagination.pages > this.page) {
+                    this.page++;
+                    this.list();
+                }
+            }
+        },
+
         load () {
             this.query = this.$route.query.q;
             this.search();
@@ -71,25 +97,41 @@ export default {
             }
 
             this.result = null;
+            this.page = 1;
+            this.resources = [];
             this.$router.replace({ name: "searcher", query: {q: this.query} })
 
-            var q = 'title ct "'+this.query+'"'
+            this.list();
+
+        },
+
+        list () {
+
+             var q = 'title ct "'+this.query+'"'
                 // +" or ("+this.query.split(" ").map(function(v) { return "title ct "+v; }).join(" or ")+")"
                 ;
 
+            this.loading = true;
 
             this.service.index({query: q, show: this.show, page: this.page}).then(response => {
                
-                
                 this.result = response.body;
+                this.resources = this.resources.concat(this.result.resources);
                 this.page = response.body.pagination.page;
                 this.show = response.body.pagination.show;
+                this.loading = false;
 
             }).catch(response => {
                 // Redirect 404
+                this.loading = false;
             });
-
         }
+    },
+    beforeMount  () {
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    beforeDestroy  () {
+        window.removeEventListener('scroll', this.handleScroll);
     },
     mounted () {
         this.service = new MangaApi();
@@ -123,7 +165,7 @@ export default {
     .cover-container {
         border: 1px solid #efefef;
         padding: 2px;
-        max-width: 100px;
+        max-width: 160px;
         width: 100%;
         margin-right: 10px;
     }
